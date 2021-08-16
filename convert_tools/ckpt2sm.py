b855d137db86f5a4d29a7b16789fca589b5d3b71
cwlroda <weiloon.c97@gmail.com>
2021-08-16 15:34:50 +0800

    Output node names to text file

--- convert_tools/ckpt2sm.py
+++ convert_tools/ckpt2sm.py
@@ -6,6 +6,15 @@ parser.add_argument("--model", type=str, default="local")
 args = parser.parse_args()
 
 
+def get_nodes(graph_def):
+    node_list = [n.name for n in graph_def.node]
+    outputs = set(node_list)
+
+    with open("nodes.txt", "w") as f:
+        for output in outputs:
+            f.write("Node Name: {}\n".format(output))
+
+
 def ckpt2sm(trained_checkpoint_prefix, export_dir):
     graph = tf.Graph()
     with tf.Session(graph=graph) as sess:
@@ -15,10 +24,23 @@ def ckpt2sm(trained_checkpoint_prefix, export_dir):
         )
         loader.restore(sess, trained_checkpoint_prefix)
 
+        graph_def = tf.get_default_graph().as_graph_def()
+        get_nodes(graph_def)
+
+        # prediction_signature = tf.saved_model.signature_def_utils.build_signature_def(
+        #     inputs={"x_input": tensor_info_x},
+        #     outputs={"y_output": tensor_info_y},
+        #     method_name=tf.saved_model.signature_constants.PREDICT_METHOD_NAME,
+        # )
+
         # Export checkpoint to SavedModel
         builder = tf.compat.v1.saved_model.builder.SavedModelBuilder(export_dir)
         builder.add_meta_graph_and_variables(
-            sess, [tf.saved_model.SERVING], strip_default_attrs=True
+            sess,
+            [tf.saved_model.SERVING],
+            # signature_def_map={
+            #     tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY: prediction_signature
+            # },
         )
         builder.save()
 
